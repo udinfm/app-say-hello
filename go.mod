8246f6ae0395b18540eff88003be06f519e71724
Very Shafrudin <very.shafrudin@gmail.com>
2022-09-05 12:36:24 +0700

    + example using own modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/udinfm/app-say-hello
+
+go 1.18
+
+require github.com/udinfm/go-say-hello v1.0.2 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	myModule "github.com/udinfm/go-say-hello"
+)
+
+func main() {
+	fmt.Println(myModule.SayHello())
+}
